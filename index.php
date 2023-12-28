ce98e8d640d717ae241c8dc9ea437adaca3e3b7f
rahmihatipoglu <112714994+rahmihatipoglu@users.noreply.github.com>
2023-12-28 22:46:07 +0300

    Update index.php
    il_id yerine id gönderilecek

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <p><a href='vue.html'>VUE Örneği</a></p>
 <p><a href='api.php?method=get.iller'>get.iller</a></p>
-<p><a href='api.php?method=get.ilceler&il_id=6'>get.ilceler?il_id=6</a></p>
+<p><a href='api.php?method=get.ilceler&id=6'>get.ilceler?id=6</a></p>
