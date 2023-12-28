897bc976509385b1d868aa58d9d4d5c6e49b5d56
Nuri Akman <nuriakman@gmail.com>
2023-12-28 07:58:55 +0300

    Çalışmalara devam edildi

--- api.php
+++ api.php
@@ -10,33 +10,38 @@ if (!isset($_GET['method'])) {
 }
 
 
-
-
 switch ($_GET['method']) {
   case 'get.iller':
-    $SQL = "SELECT IL_ID, PLAKA, IL_ADI FROM il ORDER BY IL_ADI";
+    $SQL = "SELECT * FROM il ORDER BY IL_ADI";
     $SORGU = $DB->prepare($SQL);
-    $SORGU->execute();
-    $rows = $SORGU->fetchAll(PDO::FETCH_ASSOC);
-    // DD($rows);
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($rows, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    die();
     break;
 
   case 'get.ilceler':
-    // $ILID = ($_GET['il_id']) ? $_GET['il_id'] : 0;
-    $ILID = ($_GET['il_id']) ?? 0;
-    $SQL = "SELECT ILCE_ID, IL_ID, ILCE_ADI FROM ilce WHERE IL_ID = :il_id ORDER BY ILCE_ADI";
+    $ID = ($_GET['id']) ?? 0;
+    $SQL = "SELECT * FROM ilce WHERE IL_ID = :id ORDER BY ILCE_ADI";
     $SORGU = $DB->prepare($SQL);
-    $SORGU->bindParam(':il_id', $ILID);
-    $SORGU->execute();
-    $rows = $SORGU->fetchAll(PDO::FETCH_ASSOC);
-    //DD($rows);
-    header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($rows, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-    die();
+    $SORGU->bindParam(':id', $ID);
     break;
+
+  case 'get.semtler':
+    $ID = ($_GET['id']) ?? 0;
+    $SQL = "SELECT * FROM semt WHERE ILCE_ID = :id ORDER BY SEMT_ADI";
+    $SORGU = $DB->prepare($SQL);
+    $SORGU->bindParam(':id', $ID);
+    break;
+
+  case 'get.mahalleler':
+    $ID = ($_GET['id']) ?? 0;
+    $SQL = "SELECT * FROM mahalle_koy WHERE SEMT_ID = :id ORDER BY MAHALLE_ADI";
+    $SORGU = $DB->prepare($SQL);
+    $SORGU->bindParam(':id', $ID);
+    break;
+
   default:
     die("HATA: `method` adlı değişken için doğru verilerler çağırınız");
 }
+
+$SORGU->execute();
+$rows = $SORGU->fetchAll(PDO::FETCH_ASSOC);
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($rows, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
